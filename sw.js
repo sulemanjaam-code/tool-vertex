4de13f2f0945ad5d1097f0876593f4895cc05a08
sulemanjaam-code <sulemanjaam.digital@gmail.com>
2026-05-12 10:38:36 +0500

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10415922
+    "zoneId": 10995251
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
